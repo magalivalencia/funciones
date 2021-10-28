15894709e268c8f44ba2ecc00d1bf1836c086b6e
magalivalencia <91564173+magalivalencia@users.noreply.github.com>
2021-10-28 13:39:24 -0500

    .

--- funcion9.m
+++ funcion9.m
@@ -6,14 +6,15 @@
 % Version                :1
 % Usage                  :C:\Users\magali
 %                        :octave>funcion real de variable real
+
 clear
-% la funcion f, con dominio en todo R
-% calcula f(-1), f(5) y f(6)
-f=@(x) (-x+2).*(x<-1)+(x).*(-1<=x).(x).*(-1<=x).*(x<=2)+(2).*(x>2):
+% graficar la siguiente funcion si f: D --> i |f(x)=(-x+2 si x<-1)(x si -1<=x<=2)+(2 si x>2);
+x=[-10,10];
+f=@(x)(-x+2).*(x<-1)&(x).*(-1<=x).*(x<=2)+(2).*(x>2);
 f(-1)
 f(5)
 f(6)
 % Estudia la continuidad de f
 f(3)
 f(5)
-fplot(@(x) (-x+2).*(x<-1)+(x).*(-1<=x).(x).*(-1<=x).*(x<=2)+(2).*(x>2),[-5,5]
+fplot(@(x)(-x+2).*(x<-1)+(x).*(-1<=x).*(-1<=x).*(x<=2)+(2).*(x>2));
